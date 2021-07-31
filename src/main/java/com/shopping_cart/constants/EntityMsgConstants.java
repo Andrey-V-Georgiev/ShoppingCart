5d0103ab503cb528a69f7c476a2bc18449918a41
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2021-07-31 16:25:40 +0300

    v4.0.2(User email regex fixed)

--- src/main/java/com/shopping_cart/constants/EntityMsgConstants.java
+++ src/main/java/com/shopping_cart/constants/EntityMsgConstants.java
@@ -20,7 +20,6 @@ public class EntityMsgConstants {
     public static final String PRODUCT_DESCRIPTION_NOT_NULL = "Product description cannot be null";
 
     public static final String PRODUCT_PRICE = "Product price must be at least one";
-    public static final String PRODUCT_PRICE_NOT_EMPTY = "Product price cannot be empty field";
     public static final String PRODUCT_PRICE_NOT_NULL = "Product price cannot be null";
 
     /* User */
@@ -32,7 +31,7 @@ public class EntityMsgConstants {
     public static final String USER_EMAIL_NOT_EMPTY = "User email cannot be empty field";
     public static final String USER_EMAIL_NOT_NULL = "User email cannot be null";
     public static final String USER_EMAIL_REGEX_MSG = "Not valid email";
-    public static final String USER_EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+(?:\\\\.[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\\\.[a-zA-Z0-9-]+)*$";
+    public static final String USER_EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&’*+\\/=?`{|}~^.-]{3,}@[a-zA-Z0-9.-]{3,}\\.[a-zA-Z0-9.-]{2,}$";
 
     public static final String PASSWORD_NOT_EMPTY = "Password cannot be empty field";
     public static final String PASSWORD_NOT_NULL = "Password cannot be null";
